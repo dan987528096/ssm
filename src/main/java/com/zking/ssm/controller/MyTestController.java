1b28990e7e4105b924422d38978a0c615c4baebe
dan987528096 <987528096@qq.com>
2020-07-28 10:20:16 +0800

    2020-07-28 10:20提交的代码

---
+++ src/main/java/com/zking/ssm/controller/MyTestController.java
@@ -0,0 +1,10 @@
+package com.zking.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyTestController {
+    public static void main(String[] args) {
+        System.out.println("idea的版本控制");
+    }
+}
