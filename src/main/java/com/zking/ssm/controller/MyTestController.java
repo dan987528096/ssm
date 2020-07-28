538f75b64d5cfba6ff50fb7957f06590ee0d11c5
dan987528096 <987528096@qq.com>
2020-07-28 10:24:47 +0800

    2020-07-28 10:23修改的代码

--- src/main/java/com/zking/ssm/controller/MyTestController.java
+++ src/main/java/com/zking/ssm/controller/MyTestController.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Controller;
 public class MyTestController {
     public static void main(String[] args) {
         System.out.println("idea的版本控制");
+        System.out.println("我10:23已修改文件");
     }
 }
